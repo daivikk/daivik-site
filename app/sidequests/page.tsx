e5ef215de85c2f6bd4fff1a89e3e4cc6058db03b
daivikk <daivik.d.patel@gmail.com>
2026-05-08 21:39:47 -0400

    fixed arrow

--- app/sidequests/page.tsx
+++ app/sidequests/page.tsx
@@ -14,10 +14,10 @@ export default function Sidequests() {
             href="https://www.youtube.com/watch?v=YNbq2NBES6c"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-[#333] hover:text-black transition-colors no-underline inline-flex items-center gap-0.5 group italic"
+            className="text-[#333] hover:text-black transition-colors no-underline group italic"
           >
             <em>selected to address the rutgers honors college class of 2026 alongside my twin brother</em>
-            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" className="text-[#999] transition-transform duration-300 ease-out group-hover:-translate-y-0.5">
+            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" className="inline-block align-middle ml-0.5 text-[#999] transition-transform duration-300 ease-out group-hover:-translate-y-0.5">
               <path fill="currentColor" d="M18 7.05a1 1 0 0 0-1-1L9 6a1 1 0 0 0 0 2h5.56l-8.27 8.29a1 1 0 0 0 0 1.42a1 1 0 0 0 1.42 0L16 9.42V15a1 1 0 0 0 1 1a1 1 0 0 0 1-1Z"/>
             </svg>
           </a>
